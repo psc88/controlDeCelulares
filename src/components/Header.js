b22004fbb0dc1e3ba03beed7cfa0ca265d4ce7f0
Pablo Castillo <psc88sdrd@gmail.com>
2021-09-16 23:47:56 -0300

    Primer commit creando estructura de componentes header, home, footer

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h1>Encabezamiento</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>Esto es el home</h1>
+        </div>
+    )
+}
+
+export default Home
